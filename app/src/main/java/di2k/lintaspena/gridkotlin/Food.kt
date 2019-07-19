a9403a098732eaaf4f5a1170c6965d6d4b612abb
penadidik <di2k.skh@gmail.com>
2019-07-19 11:22:18 +0700

    version 1

---
+++ app/src/main/java/di2k/lintaspena/gridkotlin/Food.kt
@@ -0,0 +1,11 @@
+package di2k.lintaspena.gridkotlin
+
+class Food {
+    var name: String? = null
+    var image: Int? = null
+
+    constructor(name: String, image: Int){
+        this.name = name
+        this.image = image
+    }
+}
---
+++ app/src/main/java/di2k/lintaspena/gridkotlin/MainActivity.kt
@@ -0,0 +1,66 @@
+package di2k.lintaspena.gridkotlin
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.food_entry.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    var adapter: FoodAdapter? = null
+    var foodList = ArrayList<Food>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //load foods
+        foodList.add(Food("Coffee", R.drawable.coffee_pot))
+        foodList.add(Food("Espresso", R.drawable.espresso))
+        foodList.add(Food("French Fires", R.drawable.french_fries))
+        foodList.add(Food("Honey", R.drawable.honey))
+        foodList.add(Food("Strawberry", R.drawable.strawberry_ice_cream))
+        foodList.add(Food("Sugar Cubes", R.drawable.sugar_cubes))
+        adapter = FoodAdapter(this, foodList)
+
+        gvFoods.adapter = adapter
+
+    }
+
+    class FoodAdapter : BaseAdapter{
+        var foodList = ArrayList<Food>()
+        var context: Context? = null
+
+        constructor(context: Context, foodList: ArrayList<Food>): super(){
+            this.context = context
+            this.foodList = foodList
+        }
+
+        override fun getCount(): Int {
+            return foodList.size
+        }
+
+        override fun getItem(position: Int): Any{
+            return foodList[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val food = this.foodList[position]
+
+            var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var foodView = inflator.inflate(R.layout.food_entry, null)
+            foodView.imgFood.setImageResource(food.image!!)
+            foodView.tvName.text = food.name!!
+
+            return foodView
+        }
+    }
+}
